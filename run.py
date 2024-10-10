2d394b3bb1c2da5e60542f07758c9d4fa9ac971a
Javier Ng <javierngkh@gmail.com>
2024-10-11 11:49:22 +1300

    Update run.py

--- run.py
+++ run.py
@@ -2,57 +2,46 @@ import RPi.GPIO as GPIO
 import time
 import datetime
 
-# Set a known date for recycling day
-# (we assume recycling one week, green waste the next)
+# Constants
 RECYCLE_START = datetime.datetime(2024, 3, 14)  # Our recycle day is on a Thursday
 GARBAGE_DAY = RECYCLE_START.weekday()
 
-# Set the pin numbers for each LED
+# Pin Configuration
 RED = 16
 YELLOW = 20
 GREEN = 21
-
-# Set the pin number for the button...
 BUTTON = 12
+LED_PINS = [RED, YELLOW, GREEN]
 
+# LED Status
+LED_STATUS = {
+    "recycling": RED,
+    "glass": YELLOW,
+    "rubbish": GREEN
+}
 
-def setup():
+# Setup GPIO
+def setup_gpio():
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-    set_GPIO_OUT()
-    all_off()
-    set_BUTTON_IN()
+    GPIO.setup(LED_PINS, GPIO.OUT)
+    GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(BUTTON, GPIO.FALLING, callback=button_clicked, bouncetime=200)
 
-
-def set_GPIO_OUT():
-    GPIO.setup([RED, YELLOW, GREEN], GPIO.OUT)
-
-
+# LED Control Functions
 def switch_on(colours):
-    set_GPIO_OUT()
     GPIO.output(colours, GPIO.HIGH)
 
-
 def switch_off(colours):
-    set_GPIO_OUT()
     GPIO.output(colours, GPIO.LOW)
 
-
 def all_off():
-    set_GPIO_OUT()
-    switch_off([RED, YELLOW, GREEN])
+    switch_off(LED_PINS)
 
-
-def set_BUTTON_IN():
-    GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(BUTTON, GPIO.FALLING, callback=button_clicked,
-                          bouncetime=200)
-
-
-def flash_LEDs(colour, flash_rate):
+def flash_leds(colour, flash_rate):
     switch_on([RED, colour])
     if flash_rate > 0:
-        for _ in xrange(flash_rate):
+        for _ in range(flash_rate):
             time.sleep(1.0 / flash_rate)
             switch_off([RED, colour])
             time.sleep(1.0 / flash_rate)
@@ -62,58 +51,55 @@ def flash_LEDs(colour, flash_rate):
     time.sleep(2)
     all_off()
 
-
+# Flashing Logic for Bin Collection
 def recycling_week(flash_rate):
-    print "Recycling Week"
-    flash_LEDs(YELLOW, flash_rate)
+    print("Recycling Week")
+    flash_leds(YELLOW, flash_rate)
 
+def green_waste_week(flash_rate):
+    print("Green Waste Week")
+    flash_leds(GREEN, flash_rate)
 
-def greenwaste_week(flash_rate):
-    print "Green waste Week"
-    flash_LEDs(GREEN, flash_rate)
-
-
+# Button Callback
 def button_clicked(pin_num):
-    # First let's stop checking for the button press...
-    # We'll add the event detect back in after checking...
     GPIO.remove_event_detect(BUTTON)
-
     check_bin_day()
-    # time.sleep(0.01) # Sleep a lil to avoid detecting phantom button presses...
-    set_BUTTON_IN()
+    setup_gpio()  # Re-setup button event detection
 
-
+# Check Bin Day Logic
 def check_bin_day():
-    # Let's find out if tonight is bin night...
     today = datetime.datetime.today()
-
-    print "Is it bin day today?"
-
     today_weekday = today.weekday()
-
     flash_rate = 0
-    if (today_weekday == GARBAGE_DAY):
+
+    if today_weekday == GARBAGE_DAY:
         flash_rate = 10
-        print "Your bins should have gone out this morning!"
+        print("Your bins should have gone out this morning!")
 
-    if ((today_weekday + 1) % 7 == GARBAGE_DAY):
+    elif (today_weekday + 1) % 7 == GARBAGE_DAY:
         flash_rate = 5
-        print "Put your bins out tonight!"
+        print("Put your bins out tonight!")
 
-    weeks_since_start = ((today - datetime.timedelta(days=1)) - RECYCLE_START).days / 7
+    weeks_since_start = (today - RECYCLE_START).days // 7
 
-    if ((weeks_since_start % 2) == 1):
+    if weeks_since_start % 2 == 1:
         recycling_week(flash_rate)
     else:
-        greenwaste_week(flash_rate)
+        green_waste_week(flash_rate)
 
-    # Now make sure all LEDs are off
-    all_off()
+    all_off()  # Ensure all LEDs are off after processing
 
+# Main Logic
+def main():
+    setup_gpio()
+    print("Press the button to check what bins go out this week!")
 
-setup()
+    try:
+        while True:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        all_off()  # Turn off LEDs on exit
+        GPIO.cleanup()
 
-print "Press the button to check what bins go out this week!"
-
-while True:
-    time.sleep(0.1)
+if __name__ == "__main__":
+    main()
